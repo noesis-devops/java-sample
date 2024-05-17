1dc6f8aa9049399d2c237e26afec301f27f7eaa2
Leandro <leandro.f.sousa@noesis.pt>
2024-05-17 12:02:15 +0100

    Refactor while loop in freelancer.js

--- WebContent/js/freelancer.js
+++ WebContent/js/freelancer.js
@@ -46,48 +46,5 @@
 
 let k = 0;
 let b = true;
-while (b) {
-    k++;    
-}
-while (b) {
-    k++;    
-}
-while (b) {
-    k++;    
-}
-while (b) {
-    k++;    
-}
-while (b) {
-    k++;    
-}
-while (b) {
-    k++;    
-}
-while (b) {
-    k++;    
-}
-while (b) {
-    k++;    
-}
-while (b) {
-    k++;    
-}
-while (b) {
-    k++;    
-}
-while (b) {
-    k++;    
-}
-while (b) {
-    k++;    
-}
-while (b) {
-    k++;    
-}
-while (b) {
-    k++;    
-}
-while (b) {
-    k++;    
-}
+
+
