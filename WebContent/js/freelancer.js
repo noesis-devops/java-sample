5fb8978c619466f98b5bbdd2b7dd1cd899ea89cb
Leandro <leandro.f.sousa@noesis.pt>
2024-05-17 12:29:32 +0100

    Refactor while loop in freelancer.js and update version in pom.xml

--- WebContent/js/freelancer.js
+++ WebContent/js/freelancer.js
@@ -46,5 +46,48 @@
 
 let k = 0;
 let b = true;
-
-
+while (b) {
+    k++;    
+}
+while (b) {
+    k++;    
+}
+while (b) {
+    k++;    
+}
+while (b) {
+    k++;    
+}
+while (b) {
+    k++;    
+}
+while (b) {
+    k++;    
+}
+while (b) {
+    k++;    
+}
+while (b) {
+    k++;    
+}
+while (b) {
+    k++;    
+}
+while (b) {
+    k++;    
+}
+while (b) {
+    k++;    
+}
+while (b) {
+    k++;    
+}
+while (b) {
+    k++;    
+}
+while (b) {
+    k++;    
+}
+while (b) {
+    k++;    
+}
